7335a4dd3cd7bce766d5d135d624c2c8ad42dec2
ximitiejiang <ximitiejiang@163.com>
2018-10-21 11:43:07 +0800

    update basics

---
+++ 0_Basics/pytorch2_module_function_layer.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 20 16:11:09 2018
+
+@author: ubuntu
+"""
+
+
+'''--------------------------------------------------------
+Q. 如何定义Module? 
+-----------------------------------------------------------
+'''
+import torch
+import torch.nn as nn
+
+# 创建模型方法1
+class AAA(torch.nn.Module):
+    def __init__(self):
+        super(Module, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5)
+        self.conv2 = nn.Conv2d(20, 20, 5)
+        
+    def forward(self, x):
+        x = conv1(x)
+        x = nn.ReLU(x)
+        x = conv2(x)
+        x = nn.ReLU(x)
+        
+net = AAA()
+
+# 创建模型方法2：默认是用list的方式存放各层，调用各层就是对list切片
+model = nn.Sequential(
+          nn.Conv2d(1,20,5),
+          nn.ReLU(),
+          nn.Conv2d(20,64,5),
+          nn.ReLU()
+        )
+for i in range(len(model)):
+    print(model[i])
+
+# 创建模型方法3: 优点是对模型取名存成字典，调用各层就是对dict切片
+from collections import OrderedDict
+model = nn.Sequential(OrderedDict([
+          ('conv1', nn.Conv2d(1,20,5)),
+          ('relu1', nn.ReLU()),
+          ('conv2', nn.Conv2d(20,64,5)),
+          ('relu2', nn.ReLU())
+          ]))
+model.conv2
+
+
+'''--------------------------------------------------------
+Q. module定义好以后有哪些可用的属性？
+这些属性在后续的train中都会使用到
+-----------------------------------------------------------
+'''
+import torch.nn as nn    
+model = nn.Sequential(
+          nn.Conv2d(1,20,5),
+          nn.ReLU(),
+          nn.Conv2d(20,64,5),
+          nn.ReLU())
+model.modules()          # 输出模型里边所有子模型，包括sequential()模型和层模型
+model.parameters()       # 输出模型里边所有参数，含数值和尺寸
+model.state_dict()       # 输出模型状态：也即模型里边所有参数/偏置的字典
+model.train()            # 设置模型在训练模式
+model.eval()             # 设置模型在测试模式
+model.zero_grad()        # 设置模型所有参数梯度归零    
+    
+
+'''--------------------------------------------------------
+Q. 如何定义各个层? 
+-----------------------------------------------------------
+'''    
+import torch
+import torch.nn as nn
+# 卷积层： [输入层数，输出层数，默认bias=True]
+m = nn.Conv2d(16, 33, (3, 5), stride=(2, 1), padding=(4, 2))
+input = torch.randn(20, 16, 50, 100)
+output = m(input)
+
+m = nn.MaxPool2d(3, stride=2)
+m = nn.MaxPool2d((3, 2), stride=(2, 1))
+input = torch.randn(20, 16, 50, 32)
+output = m(input)
+
+m = nn.AvgPool2d(3, stride=2)
+m = nn.AvgPool2d((3, 2), stride=(2, 1))
+input = torch.randn(20, 16, 50, 32)
+output = m(input)
+
+# 非线性激活
+m = nn.Softmax()
+input = torch.randn(2, 3)
+output = m(input)
+
+m = nn.ReLU()
+input = torch.randn(2)
+output = m(input)
+
+m = nn.BatchNorm2d(100)
+m = nn.BatchNorm2d(100, affine=False)
+input = torch.randn(20, 100, 35, 45)
+output = m(input)
+
+# 直接有RNN模块和LSTM模块在torch.nn中
+
+# 线性模块: [输入层数，输出层数，默认bias=True]
+m = nn.Linear(20, 30)
+input = torch.randn(128, 20)
+output = m(input)
+print(output.size())
+
+# dropout层：[关闭概率p]
+m = nn.Dropout2d(p=0.2)
+input = torch.randn(20, 16, 32, 32)
+output = m(input)
+
+
+'''--------------------------------------------------------
+Q. 如何设计一个迁移模型? 
+-----------------------------------------------------------
+'''  
+import torch
+import torch.nn as nn
+from torchvision import models
+
+# 定义resnet18模型
+model = models.resnet18(pretrained=True)
+print(model)
+in_fc = model.fc.in_features # 获得自定义fc层的输入特征层数
+model.fc = nn.Linear(in_fc, 2) # 自定义fc层, 输出为2个分类
+
+# 定义vgg16模型
+model = models.vgg16(pretrained=True)
+print(model)
+in_fc = model.classifier[6].in_features
+model.classifier[6] = nn.Linear(in_fc, 2)  # 自定义fc层，输出为2分类
+
+    
+'''--------------------------------------------------------
+Q. 如何设计一个resnet18? 
+-----------------------------------------------------------
+'''  
+import torch
+import torch.nn as nn
+
+class Resnet(nn.Module):
+    def __init__(self):
+        super(Module, self).__init__()
+        
+        
+    def forward(x):
+        
+
+
+
---
+++ 0_Basics/pytorch4_dataset_model_transform.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 20 16:13:21 2018
+
+@author: ubuntu
+"""
+
+
+'''--------------------------------------------------------
+Q. 如何定义对图片预处理的transform？
+-----------------------------------------------------------
+'''
+# 先显示原始图片
+from PIL import Image
+import matplotlib.pyplot as plt
+root = '/Users/suliang/MyDatasets/DogsVSCats/train/dog.7014.jpg'
+data = Image.open(root)
+plt.imshow(data)
+
+from torchvision import transforms
+
+# transform1: 缩放和改尺寸, resize(等效于scale, scale已经废弃用resize替代了)
+transform1 = transforms.Compose([transforms.Resize([64,64])])  # 改为指定尺寸HxW，如果1个数则只改短边但保持长宽比不变
+new_data = transform1(data)
+plt.imshow(new_data)
+
+# transform2: 切割, CenterCrop
+transform2_1 = transforms.Compose([transforms.CenterCrop((224,400))])  # 基于中心点切出HxW图片
+new_data = transform2_1(data)
+plt.imshow(new_data)
+transform2_2 = transforms.Compose([transforms.RandomResizedCrop(224)]) # 随机切，然后再扩展成size尺寸
+new_data = transform2_2(data)
+plt.imshow(new_data)
+
+# transform3: 翻转, RandomHorizontalFlip
+transform3 = transforms.Compose([transforms.RandomHorizontalFlip()])  # 随机（0.5的概率）水平翻转
+new_data = transform3(data)
+plt.imshow(new_data)
+
+# transform4: 变为张量，ToTensor
+# - 一方面把图片HxWxC，转换为张量的CxHxW
+# - 另一方面把图片(0,255)，转换为张量(0,1)
+transform4_1 = transforms.Compose([transforms.ToTensor()])  # 图像转成tensor
+PtoT = transform4(data)
+print('tensor size: {}'.format(PtoT.shape))
+print(PtoT.min(), PtoT.max())
+
+transform4_2 = transforms.Compose([transforms.ToPILImage()])  # tensor转成图像- 也可直接用transpose
+TtoP = transform4_2(PtoT)
+plt.imshow(TtoP)
+
+TtoP = np.transpose(PtoT, (1,2,0))  # ToPILImage所做的事情跟transpose是一样的
+plt.imshow(TtoP)
+
+# transform5: 归一化，一般归一化到(-1,1), 因为
+# new_value = (value - mean)/std, 后续恢复图片也要进行归一化的逆操作
+# 归一化只针对tensor，所以ToTensor与Normalize一般一起做。
+# 由于totensor已经把value转换到(0,1), 为了标准化到(-1,1)需要合适的均值/方差
+# 简化处理是设置mean=0.5, std=0.5，即(value-0.5)/0.5得到(-1,1)的区间
+transform5 = transforms.Compose([transforms.ToTensor(),
+                        transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5])])
+new_data = transform5(data)
+print('tensor size: {}'.format(new_data.shape))
+print(new_data.min(), new_data.max())
+print('mean: {}, std: {}'.format(new_data.mean(), new_data.std()))
+
+# 标准化以后的数据转换回来再显示
+new_data = new_data*0.5 + 0.5
+#TtoP = np.transpose(new_data, (1,2,0)) 
+TtoP = transforms.ToPILImage()(new_data) # 注意这种蛋疼写法，T.xxx要么嵌套在compose()里，要么多一对括号
+plt.imshow(TtoP)
+
+# transform6: 图像颜色高斯波动
+
+
+'''--------------------------------------------------------
+Q. 如何利用torchvision的datasets导入数据？
+- 首先还是下载数据集，然后可以利用pytorch统一化的API来方便地导入这些数据集
+- 可用数据集的API：
+    MNIST
+    Fashion-MNIST
+    EMNIST
+    COCO
+        Captions
+        Detection
+    LSUN
+
+    Imagenet-12
+    CIFAR10
+    CIFAR100
+    STL10
+    SVHN
+    PhotoTour
+-----------------------------------------------------------
+'''
+from torchvision import datasets, transforms
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize(mean=[0.5,0.5,0.5],
+                                                     std=[0.5,0.5,0.5])])
+# 导入CIFAR10
+trainset = datasets.CIFAR10(root = '/Users/suliang/MyDatasets/CIFAR10/',
+                            transform = transform,
+                            train = True,
+                            download = True)
+testset = datasets.CIFAR10(root = '/Users/suliang/MyDatasets/CIFAR10/',
+                           transform = transform,
+                           train = False,
+                           download = True)
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 
+           'dog', 'frog', 'horse', 'ship', 'truck')
+data, labels = trainset[0]
+
+'''--------------------------------------------------------
+Q. 如何导入非标数据集？
+- 使用如下两个通用API：
+    ImageFolder
+    DatasetFolder
+-----------------------------------------------------------
+'''
+
+
+
+
+
+
+
+'''--------------------------------------------------------
+Q. 如何分包数据？
+-----------------------------------------------------------
+'''
+# 分包数据
+trainloader = torch.utils.data.DataLoader(trainset, 
+                                          batch_size = 4,
+                                          shuffle = True,
+                                          num_workers = 2)
+testloader = torch.utils.data.DataLoader(testset, 
+                                          batch_size = 4,
+                                          shuffle = True,
+                                          num_workers = 2)
